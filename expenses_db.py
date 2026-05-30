89d632c9a010dac28285927b5ea5eadcbc44ba9f
WatanabeTough <wataworking5265@gmail.com>
2026-05-30 16:05:02 +0900

    20260530 開発ブランチ作成

--- expenses_db.py
+++ expenses_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 #db = SessionLocal()でここでselect,insert,delete,updateができる
-EXPENSES_DATABASE_URL = "sqlite:///./expenses.db" #sqliteのdbでexpenses.dbというファイルを作る
+EXPENSES_DATABASE_URL = "sqlite:///./expenses_develop.db" #sqliteのdbでexpenses.dbというファイルを作る
 
 expenses_engine = create_engine(EXPENSES_DATABASE_URL,echo=False) #engineはsqliteとpythonの中継役
 ExpensesSessionLocal = sessionmaker(bind=expenses_engine) #DB操作するためのインスタンスを作成
--- master_db.py
+++ master_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 #db = SessionLocal()でここでselect,insert,delete,updateができる
-MASTER_DATABASE_URL = "sqlite:///./master.db" #sqliteのdbでexpenses.dbというファイルを作る
+MASTER_DATABASE_URL = "sqlite:///./master_develop.db" #sqliteのdbでexpenses.dbというファイルを作る
 
 master_engine = create_engine(MASTER_DATABASE_URL,echo=False) #engineはsqliteとpythonの中継役
 MasterSessionLocal = sessionmaker(bind=master_engine) #DB操作するためのインスタンスを作成
